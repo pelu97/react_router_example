475553458d19a564638690d961306d0f5bea94b5
pelu97 <pedrolpandrade@gmail.com>
2022-07-22 11:07:27 -0300

    Added dummy quotes data

---
+++ src/data/DUMMY_QUOTES.tsx
@@ -0,0 +1,19 @@
+import { QuoteType } from "../types/QuoteType";
+
+export const DUMMY_QUOTES: QuoteType[] = [
+    {
+        id: "q1",
+        author: "Author 1",
+        text: "Quote Text 1"
+    },
+    {
+        id: "q2",
+        author: "Author 2",
+        text: "Quote Text 2"
+    },
+    {
+        id: "q3",
+        author: "Author 3",
+        text: "Quote Text 3"
+    }
+]
