02848679ffb834b920cd030fe97e75a781328136
pelu97 <pedrolpandrade@gmail.com>
2022-07-22 11:10:35 -0300

    Added quotes sorting

--- src/components/quotes/QuoteList.tsx
+++ src/components/quotes/QuoteList.tsx
@@ -1,8 +1,32 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from "react-router-dom";
 import { QuoteType } from "../../types/QuoteType";
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+
+
+
+function sortQuotes(quotes: QuoteType[], ascending: boolean){
+    return quotes.sort((quoteA, quoteB) => {
+        if (ascending) {
+            if(quoteA.id > quoteB.id){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        } else {
+            if(quoteA.id < quoteB.id){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }
+    });
+};
+
 
 
 interface QuoteListProps{
@@ -10,17 +34,40 @@ interface QuoteListProps{
 }
 
 function QuoteList(props: QuoteListProps){
+    const history = useHistory();
+    const location = useLocation();
+
+    const queryParams = new URLSearchParams(location.search);
+    console.log(queryParams.get("sort"));
+    const sortingAscending = queryParams.get("sort") === "asc";
+
+    const sortedQuotes = sortQuotes(props.quotes, sortingAscending);
+
+
+    function changeSortingHandler(){
+        if(sortingAscending){
+            history.push(`${location.pathname}?sort=des`);
+        }
+        else{
+            history.push(`${location.pathname}?sort=asc`);
+        }
+    }
+
+
     return (
         <Fragment>
+            <div className={classes.sorting}>
+                <button onClick={changeSortingHandler}>Sort {sortingAscending ? "Descending" : "Ascending"}</button>
+            </div>
             <ul className={classes.list}>
-                {props.quotes.map((quote) => (
-                    <QuoteItem
+                {sortedQuotes.map((quote) => {
+                    return <QuoteItem
                         key={quote.id}
                         id={quote.id}
                         author={quote.author}
                         text={quote.text}
-                        />
-                ))}
+                    />;
+                })}
             </ul>
         </Fragment>
     );
