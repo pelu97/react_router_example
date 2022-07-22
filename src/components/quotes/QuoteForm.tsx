8e8d2518b94df1f337d4c77045722f462ea3ca08
pelu97 <pedrolpandrade@gmail.com>
2022-07-22 11:11:07 -0300

    Added prompt if trying to leave page after focusing on form

--- src/components/quotes/QuoteForm.tsx
+++ src/components/quotes/QuoteForm.tsx
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState, Fragment } from 'react';
+import { Prompt } from "react-router-dom";
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -8,14 +9,15 @@ import classes from './QuoteForm.module.css';
 interface QuoteFormProps{
     isLoading: boolean,
     onAddQuote: (
-        {}: {author: string, text: string}
-    ) => {}
+        quote: {author: string, text: string}
+    ) => void
 }
 
 
 function QuoteForm(props: QuoteFormProps){
     const authorInputRef = useRef<HTMLInputElement>(null);
     const textInputRef = useRef<HTMLTextAreaElement>(null);
+    const [formFocused, setFormFocused] = useState(false);
 
     function submitFormHandler() {
 
@@ -27,31 +29,49 @@ function QuoteForm(props: QuoteFormProps){
         props.onAddQuote({ author: enteredAuthor, text: enteredText });
     }
 
-    return (
-        <Card>
-            <form className={classes.form} onSubmit={(event) => {
-                event.preventDefault();
-                submitFormHandler();
-            }}>
-                {props.isLoading && (
-                    <div className={classes.loading}>
-                        <LoadingSpinner />
-                    </div>
-                )}
 
-                <div className={classes.control}>
-                    <label htmlFor='author'>Author</label>
-                    <input type='text' id='author' ref={authorInputRef} />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='text'>Text</label>
-                    <textarea id='text' rows={5} ref={textInputRef}></textarea>
-                </div>
-                <div className={classes.actions}>
-                    <button className='btn'>Add Quote</button>
-                </div>
-            </form>
-        </Card>
+    function focusFormHandler(){
+        console.log("Focused on form");
+        if(!formFocused){
+            setFormFocused(true);
+        }
+    }
+
+    function finishFormHandler(){
+        setFormFocused(false);
+    }
+
+    return (
+        <Fragment>
+            <Prompt when={formFocused} message={(location) => {
+                return "Are you sure you want to leave? All your entered data will be lost!"
+            }}/>
+            <Card>
+                <form onFocus={focusFormHandler} className={classes.form} onSubmit={(event) => {
+                    event.preventDefault();
+                    submitFormHandler();
+                }}>
+                    {props.isLoading && (
+                        <div className={classes.loading}>
+                            <LoadingSpinner />
+                        </div>
+                    )}
+
+                    <div className={classes.control}>
+                        <label htmlFor='author'>Author</label>
+                        <input type='text' id='author' ref={authorInputRef} />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor='text'>Text</label>
+                        <textarea id='text' rows={5} ref={textInputRef}></textarea>
+                    </div>
+                    <div className={classes.actions}>
+                        <button onClick={finishFormHandler} className='btn'>Add Quote</button>
+                    </div>
+                </form>
+            </Card>
+        </Fragment>
+
     );
 };
 
