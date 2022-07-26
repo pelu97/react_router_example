5d1473d2db295daa436efb0cbbb85efbed581d42
pelu97 <pedrolpandrade@gmail.com>
2022-07-26 14:36:11 -0300

    Added lazy loading example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import React, { Suspense } from "react";
 import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 // pages
 import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
+// import QuoteDetail from "./pages/QuoteDetail";
+// import NewQuote from "./pages/NewQuote";
+// import NotFound from "./pages/NotFound";
+
 
 import Layout from "./components/layout/Layout";
 import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+// lazy loading example
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 // dummy data
 // import { DUMMY_QUOTES } from "./data/DUMMY_QUOTES";
@@ -22,20 +30,26 @@ import Comments from "./components/comments/Comments";
 function App() {
     return (
         <Layout>
-            <Routes>
-                <Route path="/" element={<Navigate replace to="/quotes"/>} />
-                <Route path="/quotes" element={<AllQuotes/>} />
-                <Route path="/quotes/detail/:id" element={<QuoteDetail/>}>
-                    <Route path="comments" element={<Comments/>} />
-                    <Route path={""} element={
-                        <div className="centered">
-                            <Link className="btn" to={"comments"}>Comments</Link>
-                        </div>
-                    } />
-                </Route>
-                <Route path="/quotes/new" element={<NewQuote/>} />
-                <Route path="*" element={<NotFound/>} />
-            </Routes>
+            <Suspense fallback={
+                <div className="centered">
+                    <LoadingSpinner/>
+                </div>
+            }>
+                <Routes>
+                    <Route path="/" element={<Navigate replace to="/quotes"/>} />
+                    <Route path="/quotes" element={<AllQuotes/>} />
+                    <Route path="/quotes/detail/:id" element={<QuoteDetail/>}>
+                        <Route path="comments" element={<Comments/>} />
+                        <Route path={""} element={
+                            <div className="centered">
+                                <Link className="btn" to={"comments"}>Comments</Link>
+                            </div>
+                        } />
+                    </Route>
+                    <Route path="/quotes/new" element={<NewQuote/>} />
+                    <Route path="*" element={<NotFound/>} />
+                </Routes>
+            </Suspense>
         </Layout>
     );
 }
